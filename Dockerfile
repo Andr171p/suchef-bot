0f580461b6e2c871903c5d8a417ec2e0debe0d64
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-05-08 15:09:27 +0500

    08.05.2025 add upgrade  pip installation in Dockerfile and crate main function to start app

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /suchef_bot
 
 COPY requirements.txt .
 
+RUN  pip install --upgrade pip
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
---
+++ main.py
@@ -0,0 +1,32 @@
+import logging
+import asyncio
+
+from aiogram import Bot
+
+from src.suchef_bot.ioc import container
+from src.suchef_bot.bot.dispatcher import create_dispatcher
+from src.suchef_bot.infrastructure.broker.app import create_faststream_app
+
+
+async def start_bot() -> None:
+    bot = await container.get(Bot)
+    dp = create_dispatcher()
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+async def start_broker() -> None:
+    faststream_app = await create_faststream_app()
+    await faststream_app.broker.start()
+
+
+async def main() -> None:
+    await asyncio.gather(
+        start_bot(),
+        start_broker()
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
